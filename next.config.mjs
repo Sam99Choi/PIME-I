7879f2d8624403d6028a3c8a1fc1843304a3ee82
Sam99Choi <sam99choi@gmail.com>
2024-06-13 20:25:59 -0300

    fizNextConfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
-import { withNextVideo } from "next-video/process";
-//const { withNextVideo } = require('nextvidep/process')
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default withNextVideo(nextConfig);
-//module.exports = withNextVideo(nextConfig)
+module.exports = nextConfig
