ed97df27c2b83b924130f91a84c998f7b36ff00b
Sam99Choi <sam99choi@gmail.com>
2024-06-13 20:31:38 -0300

    FixNextConfigII

--- src/Components/VideoComponent.jsx
+++ src/Components/VideoComponent.jsx
@@ -1,5 +1,4 @@
 'use client'
-import { useState } from 'react'
 import ReactPlayer from 'react-player'
 
 export default function VideoComponent({ onClick, key, url,}) {
@@ -12,12 +11,6 @@ export default function VideoComponent({ onClick, key, url,}) {
         </div>
     )
 }
-/*
-          <ReactPlayer height={180} width={360} controls={true} playing={false} url='https://www.youtube.com/watch?v=XW9IZfHiVZM&t=2s' />
-          <ReactPlayer height={180} width={360} controls={true} playing={false} url='https://www.youtube.com/watch?v=NZM9dui2MZo&list=RDQMtHX0LHiCIxY&start_radio=1' />  
-          <ReactPlayer height={180} width={360} controls={true} playing={false} url='https://www.youtube.com/watch?v=XW9IZfHiVZM&t=2s' />    
-          <ReactPlayer height={180} width={360} controls={true} playing={false} url='https://www.youtube.com/watch?v=XW9IZfHiVZM&t=2s' />   
-*/
 
 
 
